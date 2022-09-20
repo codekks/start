0f007ec57bc8bdb90c4470467970911721e245b6
Guest <guest-dme7mx@h25033.sgb.skir.kr>
2022-09-20 20:37:57 +0900

    vscode

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    int a,n;
-    scanf("%d/n",&a);
-    int array[a];
-    for(int i=0;i<a;i++) scanf("%d",array+i);
-    int max=array[0];
-    int min=array[0];
-    for (int j=1;j<=a;j++) {
-        if (array[j]>max) max=array[j];
-        else if (array[j]<min) min=array[j];
-        };
-    n=max*min;
-    printf("%d",n);
     return 0;
 }
 
