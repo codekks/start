5d91f7e1ac8be0a81f7f3695307696e4387f63eb
Guest <guest-dme7mx@h25033.sgb.skir.kr>
2022-09-20 23:39:49 +0900

    vscode

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <math.h>
+int ang(int t){
+    int b1=log(t);
+    int b=floor(b1);
+    char s[b+1];
+    int c;
+    sprintf(s,"%d",t);
+    for (int i=0;i<b;i++)
+         if (s[i]>s[i+1]) {c=1;}
+         else break; c=0;
+    return c;
+    }
 
 int main() {
+    int n,count=0;
+    scanf("%d",&n);
+    for(int a=0;a<=1000000;a++)
+        if (count==n) {break; printf("%d",a);}
+        else if (count!=n)
+            {if (ang(a)==1) count ++;}
     return 0;
 }
 
 
+ 
