199bf0fa2ec653d74f39ab3e0143873713d148c3
Guest <guest-xi7uzk@h25033.sgb.skir.kr>
2022-09-22 23:10:20 +0900

    vscode

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,19 @@
 
 
 int main(){
-    int m,n,k=0; 
+    int m,n;
     scanf("%d %d",&m,&n);
-    if (m==2) {m++; printf("%d\n",2);}
-    else if (m%2==0){m++;} 
-    else if (m==3) {m=3; printf("%d\n",3);}
-    int j=3;
-    while(m<=n)
-         {while(j<m)
-            {if (m%j==0) {k=0; break;}
-            else if (m%j!=0) {k=1;}
-            j=j+2;}
-         j=3;
-         if (k==1) printf("%d\n",m);
-         m=m+2;}
+    int ang[n+1]={0,};
     
-
+    for (int i=2;i*i<=n;i++)
+         if (ang[i]==0)
+             for (int j=i*i;j<=n;j+=i)
+                 ang[j]=1;
+    ang[1]=1;
+    for (int t=m;t<=n;t++)
+         if (ang[t]==0) printf("%d\n",t);
+    return 0;
 }
+
 
 
