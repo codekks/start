8e76056e53802d84a329993759dbda13a1761e05
Guest <guest-nxokjt@h25033.sgb.skir.kr>
2022-09-28 23:06:27 +0900

    vscode

--- src/main.cpp
+++ src/main.cpp
@@ -3,31 +3,61 @@
 
 
 int main(){
-    int n,count=9,a,result=1;
+    int n,count=9,a,t,result=1;
     scanf("%d",&n);
     if(n<10) printf("%d",n);
-    else if(n>=10){
-         for(int i=2;count<n;i++) {
+    else if (n==1022) printf("%lu",9876543210);
+    else if (n>1023) printf("%d",-1);
+    else if (n>=10){
+         for(int i=2;i<10;i++) {
              a=0;
-             for (int u=1;u<=i;u++){
-                 a=a+pow(10,u)*u; count++;
-             }
-             if(count==n) printf("%d",a); break;
-             for(a;a<pow(10,i+1);a++){
-                 if(count==n) printf("%d",a); break;
+             for (int u=1;u<i;u++){
+                 a=a+pow(10,u)*u;}
+             while(a<pow(10,i)){
+                 t=a;
                  for(int j=1;j<i;j++){
-                     int t=a;
                      if ((t % 10) < (t / 10) % 10) {
-                         t=t/10; result=1; 
-                        }
+                     t=t/10; result=1;}
                      else if((t % 10) >= (t / 10) % 10){
-                         a=a+pow(10,j)-a%pow(10,j); result=0; break;
-                        } 
-                    }
-                 if(result == 1) count++;
+                     int p=pow(10,j);
+                     a=(a/p+1)*p-1; 
+                     result=0; 
+                     t=a;
+                     break;}  
                 }
+                 if(result == 1) {count++;}
+                 if(count==n) {printf("%d",a); break;}
+                 a++;
+                }
+             if(count==n) break;
             }
         }
     }
 
+
+/*int main(){
+    int t,n,count=9,a=0,result=1,i=2;
+    scanf("%d",&n);
+    for (int u=1;u<i;u++){
+        a=a+pow(10,u)*u;}
+    while(a<pow(10,i)){
+        if(count==n) {printf("%d",a-1); break;}
+        t=a;
+        for(int j=1;j<i;j++){
+             if ((t % 10) < (t / 10) % 10) {
+                 t=t/10; result=1; 
+                }
+             else if((t % 10) >= (t / 10) % 10){
+             int p=pow(10,j);
+             a=(a/p+1)*p-1; 
+             result=0; 
+             t=a;
+             break;
+             }  
+        if(result == 1) {count++;}
+        }
+        a++;
+    }
+}
+*/
 
