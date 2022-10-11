ab132d31ff7677b65389969ab06a8f67e0e72ef1
test <codekks@naver.com>
2022-10-11 21:11:30 +0900

    vscode

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,24 @@
 #include <iostream>
+class Point {
+  int x, y;
+ public:
+  Point(int pos_x, int pos_y){
+      x=pos_x;
+      y=pos_y;
+}
+
+class Geometry {
+ private:
+  Point* point_array[100];
+  int num_points;
+ public:
+  Geometry() {
+    num_points = 0;
+  }
+  void AddPoint(const Point &point) {
+    point_array[num_points ++] = new Point(point.x, point.y);
+  }
+
+  int main(){
+      Point point;
+  }
