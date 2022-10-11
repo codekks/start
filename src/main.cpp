c1187bab0c97976fbd6dc84d6acddda1bf503625
test <codekks@naver.com>
2022-10-11 20:21:15 +0900

    vscode

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,8 @@
-#include <stdio.h>
-#include <math.h>
+#include <iostream>
+class Point{
+    int x,y;
 
-
-int main(){
-    int m,n;
-    scanf("%d %d",&m,&n);
-    int ang[n+1]={0,};
     
-    for (int i=2;i*i<=n;i++)
-         if (ang[i]==0)
-             for (int j=i*i;j<=n;j+=i)
-                 ang[j]=1;
-    ang[1]=1;
-    for (int t=m;t<=n;t++)
-         if (ang[t]==0) printf("%d\n",t);
-    return 0;
 }
 
 
