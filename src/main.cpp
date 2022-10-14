c7ab6af7ca4d650c005db0e9ea2d9d77c98263a3
내 이름 <you@example.com>
2022-10-14 22:05:51 +0900

    vscode

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,45 @@
 #include <iostream>
+#include <cmath>
+
 class Point {
   int x, y;
  public:
   Point(int pos_x, int pos_y){
       x=pos_x;
       y=pos_y;
-}
+  }
+};
 
 class Geometry {
  private:
   Point* point_array[100];
   int num_points;
+ 
  public:
   Geometry() {
     num_points = 0;
   }
-  void AddPoint(const Point &point) {
-    point_array[num_points ++] = new Point(point.x, point.y);
+  void AddPoint() {
+     srand(time(NULL));
+     int random_x=rand()%100;
+     int random_y=rand()%100;
+     point_array[num_points ++] = new Point(rando   m_x, random_y);
   }
+  void PrintDistance();
+  void PrintNumMeets();
+};
 
+void PrintDistance(int i,int j){
+  int distance=0;
+};
+  
+  
   int main(){
-      Point point;
+      int i,j;
+      Geometry geometry; 
+      for (int t=0;t<100;t++){
+        geometry.AddPoint();
+      }
+  std::cin>>i>>j;
+  std::cout<<*geometry.point_array[i]
   }
