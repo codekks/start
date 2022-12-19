dfc848daca8d532dfc9b5202745d1181fae3f7ec
Guest <guest-z8argg@h24239.sgb.skir.kr>
2022-12-19 21:25:00 +0900

    vscode

--- src/main.cpp
+++ src/main.cpp
@@ -4,93 +4,55 @@
 #include <ctime>
 #include <cstring>
 
-class Complex{
-private:
-   double real, img;
 
 
-public:
-Complex(double real, double img) : real(real), img(img) {}
-Complex(const char *str);
-Complex(const Complex& c){real = c.real, img = c.img;} ;
+class Array{
+  private:
+  const int dim;
+  int* size;
+  struct Address {int level; void* next_Address;};
 
-Complex operator+(const Complex& c) const;
-Complex operator-(const Complex& c) const;
-Complex operator*(const Complex& c) const;
-Complex operator/(const Complex& c) const;
-Complex& operator=(const Complex& c);
-void pl(){std::cout << "( " << real << " , " << img << " ) " << std::endl;}
-};
-Complex::Complex(const char *str){
  
-  int len=strlen(str);
-  int i_num=-1;
-  int o_num=-1;
-  
-  for (int i=0;i<len;i++){
-    if(str[i]=='i') {i_num=i; break;}
-    else i_num=-1;}
-  for (int i=1;i<len;i++){
-    if(str[i]=='+'||str[i]=='-') {o_num=i; break;} 
-    else o_num=-1;}
-
-  if(i_num==0){
-    char str_n[len];
-    for(int i=1;i<len;i++){
-      str_n[i]=str[i-1];
-    }
-    img=atof(str_n);
+  public:
+  Array(int dim,int* size_):dim(dim){
+    size = new int*[dim];
+    for (int i=0;i<dim;i++) size=size_;
+    top=new Address;
+    top->level=0;
   }
-  else if(i_num==-1) {real = atof(str) ;}
-  else if(i_num>0){
-    char str_n1[len];
-    char str_n2[len];
-    for(int i=0;i<i_num-1;i++){
-      str_n1[i]=str[i];
+  void make_string(const Address* a){
+    if(!a) return;
+    if (a->level==dim-1){
+      next_Address=new int*[size[dim-1]];
+      return;
     }
-    for(int i=i_num+1;i<len;i++){
-      str_n2[i-i_num-1]=str[i];
+    a->next_Address=new Address*[size[a->level]]
+    for(i=0;i<size[a->level];i++]){
+      (static<Address*>(a->next_Address)+i)->level=a->level+1;
+      make_string(static_cast<Address*>(a->next_Address)+i);
     }
-    real=atof(str_n1);
-    
-    img=atof(str_n2);
-  
-    if(str[i_num-1]=='-'){img=-img;}
   }
-
-}
-Complex Complex::operator+(const Complex& c) const {
-  Complex temp(real + c.real, img + c.img);
-  return temp;
-}
-Complex Complex::operator-(const Complex& c) const {
-  Complex temp(real - c.real, img - c.img);
-  return temp;
-}
-Complex Complex::operator*(const Complex& c) const {
-  Complex temp(real * c.real - img * c.img, real * c.img + img * c.real);
-  return temp;
-}
-Complex Complex::operator/(const Complex& c) const {
-  Complex temp(
-      (real * c.real + img * c.img) / (c.real * c.real + c.img * c.img),
-      (img * c.real - real * c.img) / (c.real * c.real + c.img * c.img));
-  return temp;
-}   
-
-Complex& Complex::operator=(const Complex& c) {
-  real = c.real;
-  img = c.img;
-  return *this;
-}
-    
-int main(){
-  Complex a(0,0);
-  Complex b = Complex("-1.1+i3.923");
-  b.pl();
-  a = a - "1.2-i1.823";
-  a.pl();
-}
+  void delete_string(Address* a){
+    for(int i=0;a->level<dim-1&&i<size[a->level];i++){
+      delete_string((static_cast<Address*>(a->next_Address)+i))
+    }
+    if(a->level==dim-1){delete[] static_cast<int *>(current->next);}
+    delete[] static_cast<Address*>(a->next);
+  }
+  void copy_string(Address* b,){
+    if (b->level==dim-1){
+      for(i=0;i<size[dim-1];i++) {
+        static_cast<int*>(b->next)[i]=}
+    for(i=0;i<size[b->level];i++){
+      b->next_Address=new Address*[]
+    }
+  }
+  ~Array(){
+    delete_string(top);
+    delete[] size;
+  }
+  
+};
 
 
   
