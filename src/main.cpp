e86991f6fd513719497dd62b921b9fe75bbd18ee
Guest <you@example.com>
2023-03-27 22:29:00 +0900

    vscode

--- src/main.cpp
+++ src/main.cpp
@@ -2,34 +2,73 @@
 #include <cmath>
 #include <fstream>
 #include <ctime>
-#include <cstring>
+#include <string>
 
-class human{
-private:
-int age;
-std::string name;
-public:
- human(const std::string name, int age):name(name),age(age){}
- std::string get_info(){
-  return "Name:"+name+"/Age:"+age;
-}
- friend std::ofstream& operater<<(std::ofstream &o,human &h);
+class Table{
+  Cell*** data_base;
+  int cell_x;
+  int cell_y;
+  int num_cellx;
+  int num_celly;
+ public:
+  Table(int cell_x, int cell_y):cell_x(cell_x),cell_y(cell_y){
+    num cellx, num_celly=0;
+    int **data_base=new int*[cell_x]
+    for(int i=0;i<cell_x;i++){
+      data_base[i]=new int[cell_y];
+    }
+  }
+  void add_cell(Cell*cell){
+    data_base[num_cellx][num_celly]=cell;
+    num_celly++;
+    if(num_celly>cell_y){
+      num_celly=0;
+      num_cellx++;
+    }
+  }
+  virtual std::string print_table(int x,int y){
+    data_base[x][y]->show_cell();
+  }
+  
+  ~Table(){
+    for(int i=0;i<cell_alloc/10;i++){
+      delete[] data_base[i];}
+    delete [] data_base;
+  }
+    
 };
- 
-std::ofstream& operater<<(std::ofstream &o,human &h){
-   o<<h.get_info();
-   return o;
- }
-
-
-int main(){
-  humam h("고경수",22);
-  std::ofstream out("test.txt");
-  if(out.Is_open()){
-    out<<h>>std::endl;
-  }
-
- 
-
-  }
+ostream& operator<<(ostream&o, Table& t){
+  o<<t.print_table();
+  return o;
 }
+class Cell{
+  Table *table;
+  std::string data;
+  int x,y;
+ public:
+ Cell(const std::string &data) : data(data) {};
+ void show_cell(){
+   return std::string data;
+ }
+};
+
+class TextTable : public Table {
+  std::ostream Text("test.txt");
+  public:
+  TextTable(std::ostream Text("test.txt"),int cell_x, int cell_y):Text(Text),Table(cell_x, cell_y);
+  void print_table(int x, int y){
+    if(Text.is_open()){
+    Text << print_table(x,y);
+    }
+  }
+};
+
+"
+class CSVTable : public Table {
+  std::ostream CSV("test.csv");
+};
+
+class HTMLTable : public Table {
+  std::ostream HTML("test.html");
+};
+"
